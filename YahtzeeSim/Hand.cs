ab03864121432feb8e986368b1c319418721105b
Ben Miller <benjaminmiller229@gmail.com>
2019-06-20 16:17:25 -0400

    Initial scoring system created

---
+++ YahtzeeSim/Hand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeSim
+{
+    public class Hand
+    {
+        public static List<int> currentDice = new List<int> { };
+
+        private int totalDice = 5;
+        private static Random rnd;
+
+        public Hand()
+        {
+            rnd = new Random();
+
+            
+            for (int i = 0; i < totalDice; i++)
+            {
+                currentDice.Add(RollDice());
+            }
+            
+        }
+
+        private static int RollDice()
+        {
+            return rnd.Next(1, 7);
+        }
+
+        public String ShowHand()
+        {
+            String hand = "";
+
+            foreach (int dice in currentDice)
+            {
+                hand += dice.ToString() + " ";
+            }
+
+            return hand;
+        }
+
+        public List<int> GetDiceList()
+        {
+            List<int> diceList = new List<int> { };
+
+            foreach (int die in currentDice)
+            {
+                diceList.Add(die);
+            }
+
+            return diceList;
+        }
+    }
+}
---
+++ YahtzeeSim/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeSim
+{
+    public class Program
+    {
+        static void Exit()
+        {
+            Console.WriteLine("\nPress ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Hand myHand = new Hand();
+            Score scoring = new Score();
+
+            string hand = myHand.ShowHand();
+            Console.WriteLine(hand + "\n");
+
+            var fullCardScore = Score.ScoreFullCard(myHand);
+
+            foreach (KeyValuePair<string, int> score in fullCardScore)
+            {
+                Console.WriteLine(score.Key + " = " + score.Value);
+            }
+
+            Exit();
+        }
+    }
+}
